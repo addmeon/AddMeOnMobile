62c8ef9b024c78d2448be704d8613ac1c67d6b8a
Emil Triest <emil@triest.de>
2023-03-30 10:40:08 -0400

    signup page

--- App.tsx
+++ App.tsx
@@ -6,51 +6,19 @@
  */
 
 import React from "react";
-import type { PropsWithChildren } from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View
-} from "react-native";
+import { Platform } from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions
-} from "react-native/Libraries/NewAppScreen";
 
 import { HCESession, NFCTagType4NDEFContentType, NFCTagType4 } from "react-native-hce";
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({ children, title }: SectionProps): JSX.Element {
-  return (
-    <View>
-      <Text>
-        {title}
-      </Text>
-      <Text>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import Signup from "./Pages/Signup";
 
 
 function App(): JSX.Element {
-
   let session;
 
 
   const startSession = async () => {
+    if (Platform.OS != "android") return;
     const tag = new NFCTagType4({
       type: NFCTagType4NDEFContentType.URL,
       content: "https://emil.triest.de/contact?private=true",
@@ -67,33 +35,8 @@ function App(): JSX.Element {
     .then(res => console.log(res))
     .catch(err => console.error(err));
 
-
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Header />
-        <View>
-          <Section title="Step One">
-            Edit to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="TEST">
-            HELLOOOOO helloooo
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Signup />
   );
 }
 
---
+++ Pages/Signup.tsx
@@ -0,0 +1,78 @@
+import LoginScreen, { emailValidator, SocialButton } from "react-native-login-screen";
+import { Linking, Platform, SafeAreaView, Text, View } from "react-native";
+import React from "react";
+
+
+function Signup(): JSX.Element {
+  let email: string;
+
+  return (
+    <>
+      <SafeAreaView>
+        <View style={{ height: "100%" }}>
+
+          <LoginScreen
+            style={{ height: "100%", backgroundColor: "white" }}
+            logoImageSource={require("../assets/logos/addmeon.jpeg")}
+            onLoginPress={() => {
+              if(!emailValidator(email)) return;
+              console.log("Email signup: " + email);
+            }}
+            onSignupPress={() => {
+              return;
+            }}
+            onEmailChange={(value: string) => {
+              email = value;
+              console.log("email: ", email);
+            }}
+            loginButtonText={"Start using Add Me On"}
+            loginButtonStyle={{ backgroundColor: "hsl(265, 100%, 65%)" }}
+            disableSignup
+            disablePasswordInput
+            onPasswordChange={() => {return;}}
+            customSocialLoginButtons={
+              <>
+                <SocialButton style={{
+                  marginBottom: 10,
+                  backgroundColor: "white",
+                  borderColor: "grey", borderWidth: 0.5
+                }}
+                              imageSource={require("../assets/logos/google.png")}
+                              iconImageStyle={{ borderRadius: 50, width: 30, height: 30, backgroundColor: "white" }}
+                              text={"Continue with Google"}
+                              textStyle={{ color: "grey", marginLeft: 4 }}
+                              onPress={() => {
+                                console.log("test");
+                              }}
+                />
+                {
+                  Platform.OS === "ios" &&
+                  <SocialButton
+                    style={{ marginBottom: 10, backgroundColor: "black" }}
+                    imageSource={require("../assets/logos/apple.png")}
+                    iconImageStyle={{ borderRadius: 50, width: 40, height: 40, marginLeft: -6 }}
+                    text={"Continue with Apple ID"}
+                    textStyle={{ color: "white" }}
+                    onPress={() => {
+                      console.log("test");
+                    }} />
+                }
+              </>
+            }
+          />
+          <Text style={{padding: 10,textAlign: "center", color: "grey", fontSize: 10, marginBottom: 10}}>
+            By signing up, you acknowledge that you have read and accept the following {""}
+            <Text style={{textDecorationLine: "underline"}}
+                  onPress={() => Linking.openURL('https://google.com')}>
+              Terms & Conditions
+            </Text>
+          </Text>
+
+        </View>
+      </SafeAreaView>
+
+    </>
+  );
+}
+
+export default Signup;
